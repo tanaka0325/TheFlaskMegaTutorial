682acfb14a4ba0f5e55b8bc72ed35b43a33850c2
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-04-17 19:23:16 +0900

    Add Edit profile page

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
 from app.models import User
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 
 
 @app.before_request
@@ -83,3 +83,20 @@ def user(username):
         'body': 'Test post #2'
     }]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template(
+        'edit_profile.html', title='Edit Profile', form=form)
